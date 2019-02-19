32ba9eefc291253b83604f68e1b6bb47f198c556
Kevin <stradtkt22@gmail.com>
2019-02-19 14:58:39 -0500

    Added a form and methods

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        };
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        this.setState({
+            error: 'Something went wrong'
+        });
+    }
     render() {
         return (
             <div>
                 <h1>Join Short Lnk</h1>
-
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" name="email" placeholder="Email"/>
+                    <input type="password" name="password" placeholder="Password"/>
+                    <button>Submit</button>
+                </form>
+                <br/>
                 <Link to="/">Already have an account?</Link>
             </div>
         );       
