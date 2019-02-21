fa76e94b3774a6be11164f6542a7a59f09a65ea6
Kevin <stradtkt22@gmail.com>
2019-02-20 20:19:21 -0500

    Added items

--- client/main.js
+++ client/main.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 
 import { routes, onAuthChange } from '../imports/routes/routes';
+import { Links } from '../imports/api/links';
 
 Tracker.autorun(() => {
     const isAuthenticated = !!Meteor.userId();
     onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+    const links = Links.find().fetch();
+    console.log("New Links", links);
 });
 
 Meteor.startup(() => {
